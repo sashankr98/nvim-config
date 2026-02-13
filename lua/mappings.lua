a7c979ca8073133fbf6f6da559bd9b89d8eac4e0
Sashank Ramachandran <sashank.r98@gmail.com>
2026-02-12 21:37:32 -0500

    Added vim-tmux-navigator

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,12 +14,14 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", {})
 vim.keymap.set("n", "<leader>bd", ":bwipeout<CR>", {})
 vim.keymap.set("n", "L", ":bnext<CR>", {})
 vim.keymap.set("n", "H", ":bprev<CR>", {})
+vim.keymap.set("n", "<M-]>", ":bnext<CR>", {})
+vim.keymap.set("n", "<M-[>", ":bprev<CR>", {})
 
 -- Window actions
-vim.keymap.set("n", "<leader>k", "<C-w>k", {})
-vim.keymap.set("n", "<leader>j", "<C-w>j", {})
-vim.keymap.set("n", "<leader>h", "<C-w>h", {})
-vim.keymap.set("n", "<leader>l", "<C-w>l", {})
+-- vim.keymap.set("n", "<leader>k", "<C-w>k", {})
+-- vim.keymap.set("n", "<leader>j", "<C-w>j", {})
+-- vim.keymap.set("n", "<leader>h", "<C-w>h", {})
+-- vim.keymap.set("n", "<leader>l", "<C-w>l", {})
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>", {})
 vim.opt.splitright = true
 
